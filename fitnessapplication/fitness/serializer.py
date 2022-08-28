7f1f21d00bdb1cbc4ebb22969e7874fdb1ea9886
Abdullah <adbdb.27@gmail.com>
2022-08-28 20:54:45 +0300

    F

--- fitnessapplication/fitness/serializer.py
+++ fitnessapplication/fitness/serializer.py
@@ -6,7 +6,7 @@ from .models import Trainee
 class TraineeRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     class Meta: 
-         model= Trainee
+         model= User
          fields = ["username", "password", "first_name", "last_name"]
     def create(self, validated_data):
         username = validated_data["username"]
