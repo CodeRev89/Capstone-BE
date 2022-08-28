dcd7ab3ed0177f8c7631aef76c9713fc317d7a2e
Abdullah <adbdb.27@gmail.com>
2022-08-28 13:26:03 +0300

    models edited

--- fitnessapplication/fitness/admin.py
+++ fitnessapplication/fitness/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Category, Trainee, Trainer,Exercise,ExerciseItem,Step
+
 # Register your models here.
+
+admin.site.register(Trainer)
+admin.site.register(Trainee)
+admin.site.register(Category)
+admin.site.register(Exercise)
+admin.site.register(ExerciseItem)
+admin.site.register(Step)
--- fitnessapplication/fitness/models.py
+++ fitnessapplication/fitness/models.py
@@ -1,23 +1,60 @@
+from unicodedata import category
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
-class Trainee(User):
-    # age=models.IntegerField()
-    # height=models.IntegerField()
-    
+
+class Trainee(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True)
+    age=models.IntegerField()
+    height=models.IntegerField()
+    weight=models.IntegerField()
+    blood_type= models.CharField(max_length=250)
+    # image= models.ImageField()
     def __str__(self):
-        return self.username
+        return self.user.username
     
-class Trainer(User):
+class Trainer(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True)
     age=models.IntegerField(blank=True, null=True)
     experience=models.IntegerField(blank=True, null=True)
     specialty= models.CharField(max_length=250)
-    def __str__(self):
-        return self.username
-
     
-class TrainerWorkout(models.Model):
-    title= models.CharField(max_length=250)
+    def __str__(self):
+        return self.user.username
+
+class Category(models.Model):
+    name= models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+class Exercise(models.Model):
+    trainer = models.ForeignKey(Trainer, on_delete=models.CASCADE, null=True,related_name="exercises")   
+    name= models.CharField(max_length=250)
     short_description= models.CharField(max_length=500)
-    workout_type= models.CharField(max_length=250)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True,related_name="exercises")  
     image=models.ImageField()
+    vedio = models.URLField(max_length=250)
+
+    def __str__(self):
+        return self.name
+
+class ExerciseItem(models.Model):
+    trainee = models.ForeignKey(Trainee, on_delete=models.CASCADE, null=True,related_name="exercises")   
+    exercise= models.ForeignKey(Exercise, on_delete=models.CASCADE, null=True,related_name="items") 
+
+    def __str__(self):
+        return self.exercise.name
+
+class Step(models.Model):
+    reps =models.IntegerField(blank=False)  
+    exercise = models.ForeignKey(Exercise, on_delete=models.CASCADE, null=True,related_name="steps")  
+
+    def __str__(self):
+        return self.reps 
+
+
+
+# Chat Models 
