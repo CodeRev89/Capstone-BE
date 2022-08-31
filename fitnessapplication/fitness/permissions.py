c0f149a963fa200266b450ee3f783d2edd9d00b0
Abdullah <adbdb.27@gmail.com>
2022-08-31 09:40:33 +0300

    F

---
+++ fitnessapplication/fitness/permissions.py
@@ -0,0 +1,7 @@
+from rest_framework.permissions import BasePermission
+
+class IsOwner(BasePermission):
+    message = "You do not have permission to preform this action"
+    
+    def has_object_permission(self, request, view, obj):
+        return obj.trainee.user == request.user
